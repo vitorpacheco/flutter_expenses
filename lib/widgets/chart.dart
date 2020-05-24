efbba75f958c0c3335fc0067dfc3bdbc376331dc
Vitor Pacheco <vpacheco.costa@gmail.com>
2020-05-23 23:11:24 -0300

    Improving the overall chart

--- lib/widgets/chart.dart
+++ lib/widgets/chart.dart
@@ -34,7 +34,7 @@ class Chart extends StatelessWidget {
         'day': DateFormat.E().format(weekDay).substring(0, 1),
         'amount': totalSum,
       };
-    });
+    }).reversed.toList();
   }
 
   @override
